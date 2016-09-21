183ef3924ed04f45973582d6c5c3fca5278d9c29
Ross Hollopeter <rosshollopeter@Rosss-MacBook-Air.local>
2016-09-20 23:46:58 -0400

    adding nice async stuff to avoid issues in producer

--- src/main/JavaScript/ChatStream/twitchProducer.js
+++ src/main/JavaScript/ChatStream/twitchProducer.js
@@ -6,82 +6,83 @@ var channelService = require('./channelService');
 var env = require('../../resources/application.json')
 var timer = require('timers')
 var kafka = require('kafka-node');
+var async = require('async')
 
 // Kafka Setup
 var Producer = kafka.HighLevelProducer;
 var client = new kafka.Client(env.KAFKA_CONNECTION_STRING);
 var producer = new Producer(client);
-var curChannels = [];
+var connectedChannels = [];
 
 // Once our producer is available:
 producer.on("ready", function(){
 
-    // Connect to the Twitch.tv IRC server
     var client = new irc.Client('irc.chat.twitch.tv', env.TWITCH_USERNAME, {
         password: env.TWITCH_OAUTH
     });
 
     client.addListener('registered', function(message){
-
-        var iterCount = 0;
+        var iteration = 0;
 
         timer.setInterval(function connect(){
 
             // Get our top 1000 channels
             channelService.get500(function(err,channels){
                 if (err){
-                    console.log("There was a problem accessing Twitch REST API:", err)
+                    console.err("There was a problem accessing Twitch REST API:", err)
                 }
                 else {
-                    iterCount++;
-                    var channelNames = _.map(channels, (x) => x.channel.name)
-                    var curChannelNames = _.map(curChannels, (x) => x.channel.name)
-                    var added = _.uniqBy(_.difference(channelNames,curChannelNames),(x) => x)
-                    var addedString = _.reduce(added,(acc,x)=>acc+'#'+x+',','')
+                    iteration++;
 
-                    // console.log('remove:',removed,'add:',added)
-                    _.forEach(channels, function(updated){
-                        var match = _.findIndex(curChannels, (x) => x.channel.name === updated.channel.name)
+                    // Get channels we haven't joined yet
+                    var added = _.uniqBy(_.differenceWith(channels,connectedChannels,(a,b) => a.channel.name === b.channel.name),(x) => x.channel.name)
+                    // Get channels to disconnect from
+                    var removed = _.uniqBy(_.differenceWith(connectedChannels,channels,(a,b) => a.channel.name === b.channel.name),(x) => x.channel.name)
 
-                        if(match >= 0){
-                            curChannels.splice(match,1,updated);
+                    var joinChannels = _.map(added, (x) => function(callback){
+                        client.join('#' + x.channel.name, function(){
+                            connectedChannels.push(x);
+                            callback(null,null)
+                        })
+                    });
+
+                    var partChannels = _.map(removed, (x) => function(callback){
+                        client.part('#' + x.channel.name, function(){
+                            connectedChannels.splice(_.findIndex(connectedChannels, (y) => x.channel.name === y.channel.name),1)
+                            callback(null,null)
+                        })
+                    });
+
+                    async.parallel(joinChannels, function(err, results){
+                        if(err){
+                            console.err('There was a problem joining a channel');
                         }
                         else {
-                            curChannels.push(updated);
+                            // Update any existing channels with new data
+                            _.forEach(channels,
+                                (x) => connectedChannels.splice(_.findIndex(connectedChannels, (y) => x.channel.name === y.channel.name),1,x)
+                            );
+
+                            if (iteration > 18)
+                            async.parallel(partChannels, function(err, results){
+                                if(err){
+                                    console.err('There was a problem parting a channel');
+                                }
+                                else {
+                                    iteration = 0;
+                                }
+                            });
                         }
                     });
 
-                    if(!!addedString){
-                        client.join(addedString);
-                    }
-                    // else if (iterCount > 4){
-                    //     iterCount = 0;
-                    //
-                    //     console.log('cleanup')
-                    //     var curChannelNames = _.map(curChannels, (x) => x.channel.name);
-                    //     var removed = _.map(_.uniqBy(_.difference(curChannelNames,channelNames),(x) => x), (x) => '#'+x);
-                    //
-                    //     curChannels = channels;
-                    //     if(!!removed){
-                    //         var date = new Date().toISOString();
-                    //         _.forEach(removed,function(channel){
-                    //             client.part(channel,function(nick,reason,message){console.log(date,' left:',channel)});
-                    //         })
-                    //     }
-                    // }
-
                 }
             });
-        } , 10000);
-
-
-        // client.join('#lirik')
-        // timer.setInterval(function(){client.part('#lirik')},7000)
+        } , 20000);
     });
 
     client.addListener('message', function (from, to, message) {
         var channelName = to.slice(1,to.length);
-        var channelData = _.find(curChannels, (x) => x.channel.name === channelName)
+        var channelData = _.find(connectedChannels, (x) => x.channel.name === channelName)
         if(!channelData) return;
         var twitchMessage = {
             "Time": new Date().toISOString(),
@@ -98,7 +99,7 @@ producer.on("ready", function(){
         }];
         // console.log(twitchMessage.User, twitchMessage.Channel)
         producer.send(message, function(err, data){
-            if(err) console.log("Error sending message to Kafka:", err);
+            if(err) console.err("Error sending message to Kafka:", err);
         });
     });
 });
