46fb9bdb0753a843f8c6d4f1dd589b0fcd4d77a4
Ross Hollopeter <rosshollopeter@Rosss-MacBook-Air.local>
2016-09-29 09:44:46 -0400

    UI update

--- src/Web/app.js
+++ src/Web/app.js
@@ -3,47 +3,76 @@ var myapp = angular.module('myapp', ["highcharts-ng"]);
 
 myapp.controller('myctrl', function ($scope, $timeout, $http, $interval) {
 
-    var update = function(){
-        $http.get('http://52.23.210.67/api/overwatchopen/20160926')
+
+    var mapToViewers = function(dataPoint){
+        return [convertDate(dataPoint),dataPoint.viewers]
+    }
+
+    var mapToChatFreq = function(dataPoint){
+        return [convertDate(dataPoint),dataPoint.count]
+    }
+
+    var mapToEngagement = function(dataPoint){
+        return [convertDate(dataPoint), dataPoint.count * 2 / (dataPoint.viewers/1000)]
+    }
+
+    var convertDate = function(dataPoint){
+        var year = dataPoint.date.substring(0,4);
+        var month = dataPoint.date.substring(4,6);
+        var day = dataPoint.date.substring(6,8);
+        return new Date(year, parseInt(month) - 1, day).getTime() + dataPoint.time
+    }
+
+    $scope.currentChannel = 'reynad27'
+    $scope.currentRanking = 'mostActive'
+    $scope.currentMapping = mapToEngagement
+    $scope.top10 = [];
+
+
+    $scope.update = function(channel){
+        $scope.currentChannel = channel;
+        $http.get('http://52.23.210.67/api/' + $scope.currentChannel + '/20160929')
             .success(function(data){
-                $scope.chartConfig.title.text = data.channelViews[0].channel;
+                $scope.chartConfig.title.text = data.channelName;
                 $scope.chartConfig.series = [{
-                    id: data.channelViews[0].channel,
-                    data: data.channelViews.map(function(x){
-                        var year = x.date.substring(0,4);
-                        var month = x.date.substring(4,6);
-                        var day = x.date.substring(6,8);
-                        return [new Date(year, parseInt(month) - 1, day).getTime() + x.time,x.count]
-                    })
+                    id: data.channelName,
+                    data: data.channelViews.map($scope.currentMapping)
                 }]
+                $scope.chartConfig.options.dummy = new Date();
             })
     }
 
-    update();
+    $scope.updateTop10 = function(ranking){
+        $http.get('http://52.23.210.67/api/' + ranking + '/')
+            .success(function(data){
+                $scope.top10 = data.top;
+            });
+    }
 
-    // $http.get('http://52.23.210.67/api/overwatchopen/20160926')
-    //     .success(function(data){
-    //         $scope.chartConfig.title.text = data.channelViews[0].channel;
-    //         $scope.chartConfig.series = [{
-    //             id: data.channelViews[0].channel,
-    //             data: data.channelViews.map(function(x){
-    //                 var year = x.date.substring(0,4);
-    //                 var month = x.date.substring(4,6);
-    //                 var day = x.date.substring(6,8);
-    //                 return [new Date(year, parseInt(month) - 1, day).getTime() + x.time,x.viewers]
-    //             })
-    //         }]
-    //     })
-            // var date = data.channelViews[0].date;
-            //
-            // console.log(data.channelViews.map(function(x){
-            //     var year = x.date.substring(0,4);
-            //     var month = x.date.substring(4,6);
-            //     var day = x.date.substring(6,8);
-            //     return [new Date(new Date(year, month, day).getTime() + x.time),x.viewers]
-            // }
-            // ))
+    $scope.updateTab = function(tab){
+        $scope.currentRanking = tab.ranking;
+        $scope.updateTop10(tab.ranking);
+        $scope.currentMapping = tab.map;
+    }
 
+
+
+    $scope.update($scope.currentChannel);
+    $scope.updateTop10($scope.currentRanking);
+
+
+    $scope.tabs = [
+        {
+            name: "Most Viewed",
+            ranking: 'mostViewed',
+            map: mapToViewers
+        },
+        {
+            name: "Most Active",
+            ranking: 'mostActive',
+            map: mapToChatFreq
+        }
+    ]
 
     $scope.chartConfig = {
         options: {
@@ -61,70 +90,33 @@ myapp.controller('myctrl', function ($scope, $timeout, $http, $interval) {
                     text: '1H'
                 }, {
                     type: 'all',
-                    text: 'day'
+                    text: 'Day'
                 }],
                 inputEnabled: false,
                 selected: 0
             },
+            credits: {enabled:false},
             navigator: {
                 enabled: true
-            }
+            },
+            scrollbar: {liveRedraw: false}
         },
         series: [],
         title: {
             text: "test"
         },
-        useHighStocks: true
+        useHighStocks: true,
+        func: function(chart) {
+            $timeout(function() {
+                chart.reflow();
+            }, 0);
+        }
     }
 
-    // $scope.chartConfig.series.push({
-    //     id: 1,
-    //     data: [
-    //         [1147651200000, 23.15],
-    //         [1147737600000, 23.01],
-    //         [1147824000000, 22.73],
-    //         [1147910400000, 22.83],
-    //         [1147996800000, 22.56],
-    //         [1148256000000, 22.88],
-    //         [1148342400000, 22.79],
-    //         [1148428800000, 23.50],
-    //         [1148515200000, 23.74],
-    //         [1148601600000, 23.72],
-    //         [1148947200000, 23.15],
-    //         [1149033600000, 22.65]
-    //     ]
-    // }
-    // );
-
-
-
-        // $scope.options = {
-        //     renderer: 'area'
-        // };
-        // $scope.series = [{
-        //         name: 'Series 1',
-        //         color: 'steelblue',
-        //         data: [{x: 0, y: 23}, {x: 1, y: 15}, {x: 2, y: 79}, {x: 3, y: 31}, {x: 4, y: 60}]
-        //     }];
-
-    $interval(update, 10000)
-    //
-    // function updateDisplay(data){
-    //     switch (data.State.Code){
-    //         case 16: $scope.displayClass.panel = "panel panel-success"; break;
-    //         case 32:
-    //         case 48:
-    //         case 64:
-    //         case 80: $scope.displayClass.panel = "panel panel-danger"; break;
-    //         case 0:
-    //         default: $scope.displayClass.panel = "panel panel-default";
-    //     }
-    //     if(data.State.Code != 16){
-    //         $scope.stopDisabled = true;
-    //     }
-    //     if(data.State.Code == 16){
-    //         $scope.stopDisabled = false;
-    //     }
-    // }
-
+    $interval(function(){
+        $scope.update($scope.currentChannel)
+    }, 5000)
+    $interval(function(){
+        $scope.updateTop10($scope.currentRanking)
+    }, 6000)
 });
