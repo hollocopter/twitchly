5d136e8db114e308a61a31d857cb15e4c3d5b3c4
Ross Hollopeter <rosshollopeter@Rosss-MacBook-Air.local>
2016-09-30 11:47:52 -0400

    new analytics added

--- src/main/java/twitchly/TwitchConsumer.java
+++ src/main/java/twitchly/TwitchConsumer.java
@@ -5,24 +5,27 @@ import com.typesafe.config.Config;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.FoldFunction;
 import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.api.java.tuple.Tuple5;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.tuple.Tuple6;
+import org.apache.flink.api.java.tuple.Tuple7;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.cassandra.ClusterBuilder;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 import org.apache.flink.util.Collector;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Properties;
+import java.util.*;
+
 import org.json.JSONObject;
 import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
 import com.datastax.driver.core.Cluster;
@@ -44,6 +47,7 @@ public class TwitchConsumer {
         final Integer WindowSize = conf.getInt("WINDOW_SIZE");
         final Integer WindowSlide = conf.getInt("WINDOW_SLIDE");
 
+
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "localhost:9092");
         properties.setProperty("zookeeper.connect", "localhost:2181");
@@ -52,19 +56,19 @@ public class TwitchConsumer {
                 .addSource(new FlinkKafkaConsumer09<>(Topic, new SimpleStringSchema(), properties));
 
 
-        DataStream<Tuple6<String, String, Long, Integer, Integer, String>> counts =
+        DataStream<Tuple7<String, String, Long, Integer, Integer, String, Integer>> counts =
                 // split up the lines in pairs (2-tuples) containing: (word,1)
                 text.flatMap(new LineSplitter())
 
-                        .keyBy(new KeySelector<Tuple3<String,Integer,String>,String>() {
-                            public String getKey(Tuple3<String,Integer,String> in) { return in.getField(0); }
+                        .keyBy(new KeySelector<Tuple4<String,Integer,String,String>,String>() {
+                            public String getKey(Tuple4<String,Integer,String,String> in) { return in.getField(0); }
                         })
                         .timeWindow(Time.seconds(WindowSize), Time.seconds(WindowSlide))
-                        .apply(new Tuple6<String, String, Long, Integer, Integer, String>("","",0L, 0, 0, ""), new MyFoldFunction(), new MyWindowFunction());
+                        .apply(new Tuple7<String, String, Long, Integer, Integer, String,Integer>("","",0L, 0, 0, "",0), new AggregateMessages(), new SlidingWindowFunction());
 
 
         CassandraSink.addSink(counts)
-                .setQuery("INSERT INTO twitchly.streamStatus (channel, date, time, count, viewers, game) values (?, ?, ?, ?, ?, ?);")
+                .setQuery("INSERT INTO twitchly.streamStatus (channel, date, time, count, viewers, game, messageLength) values (?, ?, ?, ?, ?, ?, ?);")
                 .setClusterBuilder(new ClusterBuilder() {
                     @Override
                     public Cluster buildCluster(Cluster.Builder builder) {
@@ -74,35 +78,46 @@ public class TwitchConsumer {
                 .build();
 
 
+        FlinkJedisPoolConfig redisConf = new FlinkJedisPoolConfig.Builder().setHost(DNS).setPort(6379).build();
+
+        counts.addSink(new RedisSink<Tuple7<String, String, Long, Integer, Integer, String, Integer>>(redisConf, new ViewerCountMapper()));
+        counts.addSink(new RedisSink<Tuple7<String, String, Long, Integer, Integer, String, Integer>>(redisConf, new MessageCountMapper()));
+        counts.addSink(new RedisSink<Tuple7<String, String, Long, Integer, Integer, String, Integer>>(redisConf, new EngagementMapper()));
+        counts.addSink(new RedisSink<Tuple7<String, String, Long, Integer, Integer, String, Integer>>(redisConf, new SpamMapper()));
+
         // execute program
         env.execute("Twitch Chat Analysis");
     }
 
-    private static class MyFoldFunction
-            implements FoldFunction< Tuple3<String,Integer,String>, Tuple6<String, String, Long, Integer, Integer, String> > {
+    private static class AggregateMessages
+            implements FoldFunction< Tuple4<String,Integer,String,String>, Tuple7<String, String, Long, Integer, Integer, String, Integer> > {
 
-        public Tuple6<String, String, Long, Integer, Integer, String> fold(Tuple6<String, String, Long, Integer, Integer, String> acc, Tuple3<String,Integer,String> s) {
+        public Tuple7<String, String, Long, Integer, Integer, String, Integer> fold(Tuple7<String, String, Long, Integer, Integer, String, Integer> acc, Tuple4<String,Integer,String,String> s) {
             String cur0 = acc.getField(0);
             String cur1 = acc.getField(1);
             Long cur2 = acc.getField(2);
             Integer cur3 = acc.getField(3);
             Integer cur4 = s.getField(1);
             String cur5 = s.getField(2);
-            return new Tuple6<String,String,Long,Integer, Integer, String>(cur0, cur1,cur2,cur3+1,cur4,cur5);
+            Integer cur6 = acc.getField(6);
+            Integer messageLength = ((String) s.getField(3)).split("\\s+").length;
+
+            return new Tuple7<String,String,Long,Integer, Integer, String, Integer>(cur0, cur1,cur2,cur3+1,cur4,cur5,cur6+messageLength);
         }
     }
 
-    private static class MyWindowFunction
-            implements WindowFunction<Tuple6<String, String, Long, Integer, Integer, String>, Tuple6<String, String,Long, Integer, Integer, String>, String, TimeWindow> {
+    private static class SlidingWindowFunction
+            implements WindowFunction<Tuple7<String, String, Long, Integer, Integer, String, Integer>, Tuple7<String, String,Long, Integer, Integer, String, Integer>, String, TimeWindow> {
 
         public void apply(String key,
                           TimeWindow window,
-                          Iterable<Tuple6<String, String, Long, Integer, Integer, String>> counts,
-                          Collector<Tuple6<String, String, Long, Integer, Integer, String>> out) {
-            Tuple6<String,String, Long,Integer,Integer,String> next = counts.iterator().next();
+                          Iterable<Tuple7<String, String, Long, Integer, Integer, String, Integer>> counts,
+                          Collector<Tuple7<String, String, Long, Integer, Integer, String, Integer>> out) {
+            Tuple7<String,String, Long,Integer,Integer,String, Integer> next = counts.iterator().next();
             Integer count = next.getField(3);
             Integer viewers = next.getField(4);
             String game = next.getField(5);
+            Integer totalLength = next.getField(6);
 
             Calendar fromMidnight = Calendar.getInstance();
             fromMidnight.set(Calendar.HOUR, 0);
@@ -110,7 +125,7 @@ public class TwitchConsumer {
             fromMidnight.set(Calendar.SECOND, 0);
             fromMidnight.set(Calendar.MILLISECOND, 0);
 
-            out.collect(new Tuple6<String, String, Long, Integer, Integer, String>(key, new SimpleDateFormat("yyyyMMdd").format(new Date(window.getEnd())), window.getEnd()-fromMidnight.getTimeInMillis(),count, viewers, game));
+            out.collect(new Tuple7<String, String, Long, Integer, Integer, String, Integer>(key, new SimpleDateFormat("yyyyMMdd").format(new Date(window.getEnd())), window.getEnd()-fromMidnight.getTimeInMillis(),count, viewers, game, totalLength));
         }
     }
 
@@ -119,29 +134,107 @@ public class TwitchConsumer {
      * FlatMapFunction. The function takes a line (String) and splits it into
      * multiple pairs in the form of "(word,1)" (Tuple2<String, Integer>).
      */
-    public static final class LineSplitter implements FlatMapFunction<String, Tuple3<String,Integer,String>> {
+    public static final class LineSplitter implements FlatMapFunction<String, Tuple4<String,Integer,String,String>> {
 
         @Override
-        public void flatMap(String value, Collector<Tuple3<String,Integer,String>> out) {
+        public void flatMap(String value, Collector<Tuple4<String,Integer,String,String>> out) {
 
             JSONObject obj = new JSONObject(value);
-            String channel;
             Integer viewers;
-            String game;
+            String game, message, channel;
             try
             {
                 channel = obj.getString("Channel");
                 viewers = obj.getInt("Viewers");
                 game = obj.getString("Game");
+                message = obj.getString("Message");
 
             }
             catch(Exception e){
                 channel = "Error";
                 viewers = 0;
                 game = "Error";
+                message = "";
             }
-            out.collect(new Tuple3<String,Integer,String>(channel,viewers,game));
+            out.collect(new Tuple4<String,Integer,String,String>(channel,viewers,game,message));
 
         }
     }
+
+    public static class ViewerCountMapper implements RedisMapper<Tuple7<String, String, Long, Integer, Integer, String, Integer>> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.ZADD, "ViewerCount");
+        }
+
+        @Override
+        public String getKeyFromData(Tuple7<String, String, Long, Integer, Integer, String, Integer> data) {
+            return data.getField(0);
+        }
+
+        @Override
+        public String getValueFromData(Tuple7<String, String, Long, Integer, Integer, String, Integer> data) {
+            return data.getField(4).toString();
+        }
+    }
+
+    public static class MessageCountMapper implements RedisMapper<Tuple7<String, String, Long, Integer, Integer, String, Integer>> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.ZADD, "MessageCount");
+        }
+
+        @Override
+        public String getKeyFromData(Tuple7<String, String, Long, Integer, Integer, String, Integer> data) {
+            return data.getField(0);
+        }
+
+        @Override
+        public String getValueFromData(Tuple7<String, String, Long, Integer, Integer, String, Integer> data) {
+            return data.getField(3).toString();
+        }
+    }
+
+    public static class EngagementMapper implements RedisMapper<Tuple7<String, String, Long, Integer, Integer, String, Integer>> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.ZADD, "Engagment");
+        }
+
+        @Override
+        public String getKeyFromData(Tuple7<String, String, Long, Integer, Integer, String, Integer> data) {
+            return data.getField(0);
+        }
+
+        @Override
+        public String getValueFromData(Tuple7<String, String, Long, Integer, Integer, String, Integer> data) {
+            Double viewers =  ((Integer) data.getField(4)).doubleValue();
+            Double messages = ((Integer) data.getField(3)).doubleValue();
+            return Double.toString(Math.floor((messages*2/(viewers/1000))));
+        }
+    }
+
+    public static class SpamMapper implements RedisMapper<Tuple7<String, String, Long, Integer, Integer, String, Integer>> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.ZADD, "Spam");
+        }
+
+        @Override
+        public String getKeyFromData(Tuple7<String, String, Long, Integer, Integer, String, Integer> data) {
+            return data.getField(0);
+        }
+
+        @Override
+        public String getValueFromData(Tuple7<String, String, Long, Integer, Integer, String, Integer> data) {
+            Double messageLength =  ((Integer) data.getField(6)).doubleValue();
+            Double messages = ((Integer) data.getField(3)).doubleValue();
+            return Double.toString(Math.floor(messageLength/messages));
+        }
+    }
 }
+
